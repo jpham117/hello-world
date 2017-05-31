8784820db9a16713788fe67fea7175fd61433afe
jpham117 <jpham117@users.noreply.github.com>
2017-05-30 18:57:49 -0700

    2.9 Object Literals

--- scripts.js
+++ scripts.js
@@ -56,10 +56,19 @@ $(".message-box").css("border","2px solid red");
   for(var i = 0; i < works.length; ++i) {
     $("#work").append("\
       <div class='col-sm-6 col-md-3'>\
-        <img class='img-responsive' src='" + works[i] +"'>\
+        <a href='img/wip.png' class='work-img'>\
+          <img class='img-responsive' src='" + works[i].pic +"'>\
+          <span class='info'><p class='proj-title'>Title:</p> Project One </span>\
+        </a>\
       </div>\
       ");  
   };
+
+  $(".work-img").mouseenter( function() {
+    $(".info", this).show();
+  }).mouseleave( function() {
+    $(".info", this).hide();
+  });
 
   var images = $("#work img");
   if(i%2 === 0){
