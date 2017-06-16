3e3f50b2e334044e84ed5d3ace61cf403a1dcaee
jpham117 <jpham117@users.noreply.github.com>
2017-06-15 23:23:03 -0700

    3.5 OOP

--- cat.rb
+++ cat.rb
@@ -1,4 +1,4 @@
-class Cat 
+class Pet
 	attr_reader :color, :breed
 	attr_accessor :name  # attr_accessor covers both functions for :name (attr_reader & attr_writer)
 	
@@ -21,12 +21,13 @@ class Cat
 		end # end of if/else
 		@hungry
 	end # end of hungry method
+end # end pet class
 
+class Cat < Pet
 	def speak
 		puts "Meow!"
 	end # end of speak method
-
-end # end of class cat
+end # end of cat class
 
 kitty = Cat.new("grey", "Persian")
 	puts "Let's inspect our new cat:"
@@ -51,6 +52,23 @@ kitty = Cat.new("grey", "Persian")
 	kitty.feed("tuna")
 	puts "Is our cat hungry now?"
 	kitty.hungry?
-	
+
 	puts "Our cat can make noise"
 	kitty.speak
+
+class Dog < Pet
+	def speak
+		puts "Woof!"
+	end # end of speak method
+end # end of dog class
+
+puppy = Dog.new("black", "Boxer")
+	puts "What color is our dog?"
+	puts puppy.color
+
+	puts "Our dog can make noise"
+	puppy.speak
+
+	puts "Guess what breed our dog is"
+	puts puppy.breed
+	
